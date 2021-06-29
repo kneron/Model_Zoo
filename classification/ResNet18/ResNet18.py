d80707c81d39e4a63a4ed3b3ac146cae1cf08859
Ziyan <ziyan.frank.zhu@gmail.com>
2021-06-29 16:54:13 -0700

    update Resnet18 backbone

--- classification/ResNet18/ResNet18.py
+++ classification/ResNet18/ResNet18.py
@@ -5,13 +5,15 @@ import torchvision
 from torchvision import datasets, models, transforms
 
 class resnet18(nn.Module):
-    def __init__(self, num_classes=1000):
+    def __init__(self, num_classes):
         super(resnet18, self).__init__()
         self.model =  models.resnet18(pretrained=False)
         # replace the last FC layer by a FC layer for our model
         num_ftrs =  self.model.fc.in_features
         self.model.fc = nn.Linear(num_ftrs, num_classes, bias=True)
-               
+        nn.init.xavier_uniform_(self.model.fc.weight)
+        self.model.fc.bias.data.fill_(0.01)
+        
     def forward(self, x):
         f = self.model(x)
         return f
