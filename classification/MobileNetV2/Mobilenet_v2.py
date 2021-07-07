0a21676daa9ffe512a0489eef5c8bf7dc1cb1001
Ziyan <ziyan.frank.zhu@gmail.com>
2021-07-06 22:47:40 -0700

    update mobilenet

--- classification/MobileNetV2/Mobilenet_v2.py
+++ classification/MobileNetV2/Mobilenet_v2.py
@@ -8,7 +8,7 @@ from torchvision import datasets, models, transforms
 class mobilenet_v2(nn.Module):
     def __init__(self, num_classes):
         super(mobilenet_v2, self).__init__()
-        self.model = models.mobilenet_v2(pretrained=True)
+        self.model = models.mobilenet_v2(pretrained=False)
         # replace the last FC layer by a FC layer for our model
         num_ftrs =  self.model.classifier[-1].in_features
         self.model.classifier[1] = nn.Linear(num_ftrs, num_classes, bias=True)
